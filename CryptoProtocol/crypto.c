88fbe6412f3e6049c4dd38fb9bb30875a843881d
josetarsitano <tarsitano.jose@gmail.com>
2016-03-10 11:42:00 +0100

    Add group

---
+++ CryptoProtocol/crypto.c
@@ -0,0 +1,9 @@
+//
+//  crypto.c
+//  CryptoProtocol
+//
+//  Created by José Tarsitano on 10/03/2016.
+//  Copyright © 2016 Alexandre Cetto. All rights reserved.
+//
+
+#include "crypto.h"
---
+++ CryptoProtocol/crypto.h
@@ -0,0 +1,14 @@
+//
+//  crypto.h
+//  CryptoProtocol
+//
+//  Created by José Tarsitano on 10/03/2016.
+//  Copyright © 2016 Alexandre Cetto. All rights reserved.
+//
+
+#ifndef crypto_h
+#define crypto_h
+
+#include <stdio.h>
+
+#endif /* crypto_h */
